fe3a72401a1b100aa13f3559258ca8edb4b39e85
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-03-10 11:42:29 +0100

    Update ports

--- Warbud.ReverseProxy/Program.cs
+++ Warbud.ReverseProxy/Program.cs
@@ -1,20 +1,12 @@
-using System.Collections.Generic;
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using System.Text.Json;
 using Microsoft.Extensions.Hosting;
-using Warbud.Shared.Configurations;
 
 namespace Warbud.ReverseProxy
 {
     public class Program
     {
-        private const string ConfigPath = "C:/Users/afranczak/source/repos/Nairda015/Warbud/ports.json";
-        private static Dictionary<string, int> _ports;
         public static void Main(string[] args)
         {
-            var config = File.ReadAllText(ConfigPath);
-            _ports = JsonSerializer.Deserialize<Dictionary<string, int>>(config);
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -23,10 +15,7 @@ namespace Warbud.ReverseProxy
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    var port = UseUrlsConfiguration.Configure()
-                        .SetConfigPath("C:/Users/afranczak/source/repos/Nairda015/Warbud/ports.json")
-                        .GetPort("Proxy");
-                    webBuilder.UseUrls($"http://localhost:{port.ToString()}");
+                    webBuilder.UseUrls("http://localhost:1500");
                 });
     }
 }
